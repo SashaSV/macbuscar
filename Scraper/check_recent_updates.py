540d563d56d936ca81881a0b99f3a446461d76f2
sasha <sashasv83@gmail.com>
2026-08-05 17:40:39 +0300

    Remove one-off diagnostic scripts from the Chrome 151 debugging session

--- Scraper/check_recent_updates.py
+++
@@ -1,9 +0,0 @@
-﻿import sys; sys.path.insert(0, "scanner")
-import dbservice_postgres as db
-conn = db.get_connection()
-cur = conn.cursor()
-cur.execute(
-    "SELECT COUNT(*) FROM \"ScrapedProduct\" WHERE \"storeId\"=%s AND \"updatedAt\" > now() - interval %s",
-    ("worten", "2 hours")
-)
-print("updated in last 2h:", cur.fetchone())
